c0695a4e6d0bd8305a1aab2ca57ab25d70601237
Autumn Fotopoulos <acarte94@gmail.com>
2021-05-21 15:11:21 -0500

    phase 5

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿using System;
-Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+﻿
+using System;
+MooseSays("H I, I'M  E N T H U S I A S T I C !");
+MooseSays("I really am enthusiastic");
 
-Console.WriteLine("--------------------------------------------");
-
-Console.WriteLine();
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
 
 void MooseSays(string message)
 {
     Console.WriteLine($@"
-                                      _.--^^^--,
+                                       _.--^^^--,
                                     .'          `\
   .-^^^^^^-.                      .'              |
  /          '.                   /            .-._/
@@ -37,5 +38,23 @@ void MooseSays(string message)
     ");
 }
 
-MooseSays("H I, I'M  E N T H U S I A S T I C !");
-MooseSays("I really am enthusiastic");
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
